70d155cf85ee1a8c346bf7e8bdb7ab6c65a7f466
Tim Matyas <matyastr@gmail.com>
2026-02-09 11:56:38 -0500

    feat: finished first pass of feature using unsplash api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,19 +1,58 @@
+import { Component, inject, signal } from "@angular/core";
+import {
+  RefresherCustomEvent,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonRefresher,
+  IonButton,
+  IonInput,
+  IonRefresherContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonItem,
+  IonList,
+} from "@ionic/angular/standalone";
+import { MessageComponent } from "../message/message.component";
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
-import { Component, inject } from '@angular/core';
-import { RefresherCustomEvent, IonHeader, IonToolbar, IonTitle, IonContent, IonRefresher, IonRefresherContent, IonList } from '@ionic/angular/standalone';
-import { MessageComponent } from '../message/message.component';
-
-import { DataService, Message } from '../services/data.service';
+import { DataService, Message } from "../services/data.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonRefresher, IonRefresherContent, IonList, MessageComponent],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
+  imports: [
+    ReactiveFormsModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonRefresher,
+    IonRefresherContent,
+    IonList,
+    IonButton,
+    IonInput,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonItem,
+    MessageComponent,
+  ],
 })
 export class HomePage {
+  searchControl = new FormControl("");
   private data = inject(DataService);
-  constructor() {}
+
+  photos = signal<any>([]);
+
+  private accessKey = "<enter key>";
+  private searchUrl = "https://api.unsplash.com/search/photos";
+
+  constructor(private http: HttpClient) {}
 
   refresh(ev: any) {
     setTimeout(() => {
@@ -21,7 +60,31 @@ export class HomePage {
     }, 3000);
   }
 
+  // TODO: Clean up
   getMessages(): Message[] {
     return this.data.getMessages();
+  }
+
+  onSearch() {
+    const searchTerm = this.searchControl.value;
+
+    if (!searchTerm) return;
+
+    const headers = new HttpHeaders({
+      Authorization: `Client-ID ${this.accessKey}`,
+    });
+
+    const params = {
+      query: searchTerm,
+      per_page: "20",
+    };
+
+    this.http.get<any>(this.searchUrl, { headers, params }).subscribe({
+      next: (res) => {
+        console.log(res.results);
+        this.photos.set(res.results);
+      },
+      error: (err) => console.error("Unsplash Error:", err),
+    });
   }
 }
